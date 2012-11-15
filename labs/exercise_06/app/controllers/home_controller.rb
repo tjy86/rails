e6dc99095f773f53b763923ed51acaa7af87706a
tjy86 <thomasjyang@gmail.com>
2012-11-15 11:08:55 -0500

    e06

---
+++ labs/exercise_06/app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+	def a
+	end
+	def b
+	end
+	def c
+	end
+	def search
+	case params[:go].downcase
+		when 'a'
+			redirect_to a_path
+		when 'b'
+			redirect_to b_path
+		when 'c'
+			redirect_to c_path
+		end
+
+	end
+end
---
+++ labs/exercise_06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exercise06::Application.config.secret_token = 'e56afc17df210b4a01c3d8a7befea5464ea9d989d90a7d552bdf2d5378f676608e46a46a3aedb3a48e4d50851eaff871b584b1def89de755cb683ce6f4de66d3'
