798a49d96e6869043654dfb824275ea34ec29e70
tjy86 <thomasjyang@gmail.com>
2012-11-26 11:05:33 -0500

    seed file

--- labs/alienvspredator/db/seeds.rb
+++ labs/alienvspredator/db/seeds.rb
@@ -1,8 +1,14 @@
+CreatureType.delete_all
+Creature.delete_all
+
 ct1 = CreatureType.create(:name => 'Alien')
 ct2 = CreatureType.create(:name => 'Predator')
 ct3 = CreatureType.create(:name => 'Godzilla')
 ct4 = CreatureType.create(:name => 'Medusa')
 
-('Aa.Zz').each do |letter|
-  c = Creature.create(:name => '', :weapon = '', :password => 'a', :password_confirmation => 'a')
+creature_types =  [ct1,ct2,ct3,ct4]
+weapons = ['knife','gun','boomerang','magic', 'mace']
+
+('Aa'..'Zz').each do |letter|
+  c = Creature.create(:name => letter, :weapon => weapons.sample, :password => 'a', :password_confirmation => 'a', :creature_type => creature_types.sample)
 end
