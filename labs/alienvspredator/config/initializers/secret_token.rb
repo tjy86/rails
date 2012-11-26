ac911083a3206658f76101cc51b30188bb95e6ba
tjy86 <thomasjyang@gmail.com>
2012-11-26 10:07:31 -0500

    update'

---
+++ labs/alienvspredator/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alienvspredator::Application.config.secret_token = 'b6ba17cfa64c6e2e78b415f42fb044c309d1526fe693cb863fcedc32e42ef6000feb061e580ea2a5f48f3b4ef3b019643a94b7c5ce1741bb40a532e2e6d177c0'
