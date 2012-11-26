81a5aaa9f5914ed155b4724660f993813b14febd
tjy86 <thomasjyang@gmail.com>
2012-11-26 10:54:49 -0500

    seed file

--- labs/alienvspredator/db/seeds.rb
+++ labs/alienvspredator/db/seeds.rb
@@ -4,5 +4,5 @@ ct3 = CreatureType.create(:name => 'Godzilla')
 ct4 = CreatureType.create(:name => 'Medusa')
 
 ('Aa.Zz').each do |letter|
-
+  c = Creature.create(:name => '', :weapon = '', :password => 'a', :password_confirmation => 'a')
 end
