275a84966a206f6562ce4a7ac5800b71a5c66aee
tjy86 <thomasjyang@gmail.com>
2012-11-26 12:51:29 -0500

    js to add creature and controller

--- labs/alienvspredator/app/assets/javascripts/avp.js
+++ labs/alienvspredator/app/assets/javascripts/avp.js
@@ -1,5 +1,11 @@
 $(function(){
 
   $('#creature_form').hide();
+  $('#new_creature').click(show_form);
 
 });
+
+function show_form(){
+  $('#creature_form').show();
+  $('#new_creature').hide();
+}
--- labs/alienvspredator/app/controllers/creature_types_controller.rb
+++ labs/alienvspredator/app/controllers/creature_types_controller.rb
@@ -2,4 +2,8 @@ class CreatureTypesController < ApplicationController
   def index
     @creature_types = CreatureType.all
   end
+  def create
+    name = params[:name]
+    CreatureType.cretae(:name => name)
+  end
 end
