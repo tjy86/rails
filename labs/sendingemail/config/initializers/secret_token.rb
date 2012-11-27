d905d466ec89de66fe6f07aa41834bd3ccee9fd7
tjy86 <thomasjyang@gmail.com>
2012-11-27 09:45:18 -0500

    using amazon SES simple email service mailer - config file

---
+++ labs/sendingemail/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sendingemail::Application.config.secret_token = 'b84b28b96e903d104499a2c4249c52c3721ed4da30d4c54b109ed5b7ba7ad27d562e65a78a57ad623895790a4bb474eae91d6aec0d3234117327fe703fbf79cb'
---
+++ labs/sendingemail/config/initializers/ses_config.rb
@@ -0,0 +1,3 @@
+ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
+  :access_key_id     => ENV['AWSKEY'],
+  :secret_access_key => ENV['AWSSEC']
