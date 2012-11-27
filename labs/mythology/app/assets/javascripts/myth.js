349d4c7f9a428eb5ea6a1a098a4f8cfe8c16d603
tjy86 <thomasjyang@gmail.com>
2012-11-27 14:09:05 -0500

    javascript for searching

--- labs/mythology/app/assets/javascripts/myth.js
+++ labs/mythology/app/assets/javascripts/myth.js
@@ -1,9 +1,18 @@
 $(function(){
 
-$('body').keypress(pressed_key);
+  $('body').keypress(pressed_key);
+  $('#searchform').hide()
 
 });
 
-function pressed_key () {
-
+function pressed_key (key) {
+  if(key.keyCode == 6)
+  {
+    $('#searchform').show()
+    $('#searchform input').val('')
+    $('#searchform input').focus()
+  }
+  if (key.keyCode == 8) {
+    $('#searchform').hide()
+  };
 }
