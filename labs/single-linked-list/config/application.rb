b93de74551a812b738af9e1ba1a7adfa44d69eb6
tjy86 <thomasjyang@gmail.com>
2012-12-03 15:25:19 -0500

    git init single linked list

--- labs/single-linked-list/config/application.rb
+++ labs/single-linked-list/config/application.rb
@@ -9,7 +9,7 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-module StackDemo
+module SingleLinkedList
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -17,7 +17,6 @@ module StackDemo
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(#{config.root}/lib/cs)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
---
+++ labs/single-linked-list/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SingleLinkedList::Application.config.secret_token = '359eb1a43d4151af77ab6d4609ba6593949ba89315dfce3b882d247ab3006bf45b2d481a176a88e0ede4f1481dee6ad9e6836a649a2646edfa70785fb29960e6'
