522ca81043bd4146ca216ecd3950baa813422660
tjy86 <thomasjyang@gmail.com>
2012-12-03 10:35:47 -0500

    initial stack-demo

---
+++ labs/stack-demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StackDemo::Application.config.secret_token = 'f7b34b530a64703c8a6a937808240b4ec956f9f987a6fc5ba7f8f3c5620296f8bad663a87d722926093bd4303d7a1e188d5c981052d4146cfa231dec4d0e4142'
---
+++ labs/subway-map/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubwayMap::Application.config.secret_token = '8cbb27b95398a4f8212e182641ad84654b48170df38dc286a56d618c17a9da476d2206b0e8e56c87368297c4f40b504a84d00babe3b8b8b87ffe75506fac31d8'
