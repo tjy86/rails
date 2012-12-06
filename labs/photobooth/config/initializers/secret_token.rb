b715e46101d3b3760872deb071454301fcf65a90
tjy86 <thomasjyang@gmail.com>
2012-12-06 15:01:29 -0500

    photoboot init

---
+++ labs/photobooth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photobooth::Application.config.secret_token = '551dbb3a12ca8d3b8eec2d9b87f2195e113817d30cd7c4c4a258a43f4223be40e830f5b686b38c08390ee643864bdec221af6642e219f4ee3fa2d3966788473d'
