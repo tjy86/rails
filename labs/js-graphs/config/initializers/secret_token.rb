4b1f36c5e077d5e0893902906bab7e23eabb8b7c
tjy86 <thomasjyang@gmail.com>
2012-12-06 10:53:52 -0500

    js graphs

---
+++ labs/js-graphs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsGraphs::Application.config.secret_token = '1a9ddb2ab87dba26cf5e5d116993c6c7282895c15ee97875a3478d7934bfec03accf8be761c08218f521957f7787c3bacdcfa4715dcc442167c885201679dfa0'
