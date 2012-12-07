6c471f1b9b589515ae319801046aca0a5587e87c
tjy86 <thomasjyang@gmail.com>
2012-12-07 10:03:41 -0500

    stokboard init

---
+++ labs/stokboard/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stokboard::Application.config.secret_token = '280a825d718b76bc6a827f48f9ce36ac8662a39c5b1b17e0cb262d11a5e9b8f67f949d0f312f4f17a71f5762c6c875a9f7f8d5b5086c03484d5f8329d77a7ac8'
