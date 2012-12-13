3b7407b08f449b1e7d1b97f684d8efe4fe6ce01f
tjy86 <thomasjyang@gmail.com>
2012-12-13 16:13:17 -0500

    git init debug

---
+++ labs/debugging/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Debugging::Application.config.secret_token = 'ea06dbf0893f0df7675ab42bf34881a6f3da8af30af3e507b53d0bd62982532278430a70ab2353e980e4ffb97bda5be8d99c11cf9b3205dd3f94c51749d8b0c6'
