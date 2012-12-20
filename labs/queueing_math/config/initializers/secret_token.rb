1c9aebd0268c8ae5eeb8a5b5db392366064baadb
tjy86 <thomasjyang@gmail.com>
2012-12-20 13:48:15 -0500

    starting queueing lesson

---
+++ labs/queueing_math/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QueueingMath::Application.config.secret_token = '5b0a4d36c639d888095f2fcd2b5188928cfc11eb687fb7ac1dd9221aca15686397386fbba99d1e3ed371b5edb587fbe03ad7793af71570550d169db550198f67'
