46bca011443e8cfe38fa99d944ba4b52ae35296a
tjy86 <thomasjyang@gmail.com>
2012-12-21 10:52:24 -0500

    starting security features

---
+++ labs/security_features/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecurityFeatures::Application.config.secret_token = 'bb6ff8b3f5676992d34aab0a91fad516db3727236089366b78884ae09c1f6f22eab596c23574f2e7c4c3bb33ca27eea970e8fe91f4afb2618def27808010ba15'
